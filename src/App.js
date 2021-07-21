a8f4e917441a05bb504a4445d1d7b6d39635be66
JMariano88 <mmarianojimmy@gmail.com>
2021-07-21 11:12:56 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ import Hud from './Hud'
 function Model(props) {
   const group = useRef()
   useFrame(() => (group.current.rotation.y -= 0.002))
-  const { nodes, materials } = useGLTF('./worldss5.glb')
+  const { nodes, materials } = useGLTF('/worldss5.glb')
   return (
     <group ref={group} {...props} dispose={null}>
-      <mesh
-        geometry={nodes.Puzzling_Rocky_Paths.geometry}
-        material={materials['Default OBJ']}
-        position={[-3.32, -4.81, 0.82]}
-        rotation={[Math.PI / 2, 0, 0]}
-        scale={56.37}
-      />
       <mesh
         geometry={nodes.Sphere.geometry}
         material={materials['Material.001']}
