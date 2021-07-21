f9eb0d68331b4feb58d94a287614bed2fdbb1a7b
JMariano88 <mmarianojimmy@gmail.com>
2021-07-21 13:52:45 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Hud from './Hud'
 function Model(props) {
   const group = useRef()
   useFrame(() => (group.current.rotation.y -= 0.002))
-  const { nodes, materials } = useGLTF('/worldss5.glb')
+  const { nodes, materials } = useGLTF('./worldss5.glb')
   return (
     <group ref={group} {...props} dispose={null}>
       <mesh
